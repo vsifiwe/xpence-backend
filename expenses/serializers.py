5b8d97f8d26d4fa3debe4c648c3399b25931c3c0
Manzi <asifiwemanzi@gmail.com>
2021-12-31 11:08:27 +0200

    Done 100%

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -39,11 +39,12 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'phone')
+        fields = ('id', 'username', 'email', 'password',
+                  'phone', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            validated_data['username'], email=validated_data['email'], password=validated_data['password'], phone=validated_data['phone'])
+            validated_data['username'], email=validated_data['email'], password=validated_data['password'], phone=validated_data['phone'], first_name=validated_data['first_name'], last_name=validated_data['last_name'])
 
         return user
